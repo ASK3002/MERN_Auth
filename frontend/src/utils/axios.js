1700de567194f998712d6e06d10e751d7870be83
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-08-12 23:47:26 +0530

    render setting scanges axios.js and server.js and  build cmnd added in backend packahe.json

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
- baseURL: 'https://spendsense-a-s-k.onrender.com/api', // ✅ point to full backend path
+  baseURL: '/api', // ✅ point to full backend path
   withCredentials: true,               // ✅ allow cookie to be sent
 });
 
