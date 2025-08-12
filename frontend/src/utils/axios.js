5638d297418b9fa2b0b93e54d759eaf2eb7fe19f
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-08-12 23:12:00 +0530

    base url changed to render link

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ point to full backend path
+ baseURL: 'https://spendsense-a-s-k.onrender.com/api', // ✅ point to full backend path
   withCredentials: true,               // ✅ allow cookie to be sent
 });
 
