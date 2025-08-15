ef4efec08d1a27e0a81a9286e5373902f4dc945f
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-08-16 01:18:14 +0530

    link changed to render  link

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 const baseURL = 
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/api'  // dev backend URL
+    ? 'https://spendsense-7a5q.onrender.com/'  // dev backend URL
     : '/api';                      // prod relative API
 
 const instance = axios.create({
